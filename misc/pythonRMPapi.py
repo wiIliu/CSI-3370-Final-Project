7cc5a56bd9b5497ecb50327e5dc4edc53847fd33
wiIliu <willowconnelly@oakland.edu>
2023-12-02 16:24:31 -0500

    update .py script

--- misc/pythonRMPapi.py
+++ misc/pythonRMPapi.py
@@ -30,8 +30,8 @@ for PROF_NAME, PROF_ID in results:
       mycursor.execute(sql,vals)
       print(f"prof {prof.name} has a rating of {prof.rating} is added")
     else:
-      sql = (f"UPDATE professor SET PROF_RATING = NULL WHERE professor.PROF_ID = {PROF_ID};")
-      mycursor.execute(sql)
+      # sql = (f"UPDATE professor SET PROF_RATING = NULL WHERE professor.PROF_ID = {PROF_ID};")
+      # mycursor.execute(sql)
       print(f"Professor {PROF_NAME} is not rated yet")
       
 # print(f"time: {timeit.default_timer()-start:.6} s")
